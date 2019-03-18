7b83e8d11444287efdc0da96b296f678cb28fed0
flyshuai <914423648@qq.com>
2019-03-18 10:35:52 +0800

    三月18日提交

--- src/main/java/com/timecapsule/controller/IndexController.java
+++ src/main/java/com/timecapsule/controller/IndexController.java
@@ -1,36 +1,29 @@
 package com.timecapsule.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    @RequestMapping("put")
+    public String put(){
+        return "put";
+    }
 
-	@RequestMapping("put")
-	public String put() {
-		return "put";
-	}
+    @RequestMapping("destory")
+    public String destorySession(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "login";
+    }
+    @RequestMapping("textPut")
+    public String textPut(){
+        return "textPut";
+    }
 
-	@RequestMapping("destory")
-	public String destorySession(HttpServletRequest request) {
-		request.getSession().invalidate();
-		return "login";
-	}
-
-	@RequestMapping("textPut")
-	public String textPut() {
-		return "textPut";
-	}
-
-	@RequestMapping("soundPut")
-	public String soundPut() {
-		return "soundPut";
-	}
-
-	@RequestMapping("moviePut")
-	public String moviePut() {
-		return "moviePut";
-	}
+    @RequestMapping("soundPut")
+    public String soundPut(){
+        return "soundPut";
+    }
 }
--- src/main/java/com/timecapsule/controller/PutController.java
+++ src/main/java/com/timecapsule/controller/PutController.java
@@ -1,95 +1,17 @@
 package com.timecapsule.controller;
 
-import com.timecapsule.pojo.Capsule;
-import com.timecapsule.pojo.User;
-import com.timecapsule.service.CapsuleService;
-import com.timecapsule.tools.FileTool;
 import com.timecapsule.tools.JsonResult;
-import com.timecapsule.tools.OpenPassword;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Map;
-
 
 @Controller
 public class PutController {
     @Autowired
     JsonResult jsonResult;
-    @Autowired
-    Capsule capsule;
-    @Autowired
-    CapsuleService capsuleService;
-
     @RequestMapping("soundCapsulePut")
-    @ResponseBody
-    public JsonResult soundUpload(HttpServletRequest request) throws IOException {
-        //获取保存在session中的用户信息
-        User user = (User) request.getSession().getAttribute("user");
-        MultipartHttpServletRequest mpRequest = (MultipartHttpServletRequest) request;
-        MultipartFile file = mpRequest.getFile("file");
-        String path = FileTool.uploadFile(file,user);
-        capsule.setCapsulePath(path);
-        capsule.setCapsuleName(request.getParameter("capsuleName"));
-        capsule.setName(request.getParameter("name"));
-        capsule.setEmail(request.getParameter("email"));
-        capsule.setUserId(user.getUserId());
-        capsule.setCapsuleOpenTime(request.getParameter("openTime"));
-        Date date = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");//格式都为2019-03-18
-        capsule.setCapsuleUploadTime(simpleDateFormat.format(date));
-        capsule.setCapsuleTypeId(1);
-        capsule.setOpenPassword(OpenPassword.buildOpenPassword());
-        System.out.println(capsule.toString());
-        if (capsuleService.insertSoundCapsule(capsule)==1){
-            jsonResult.setStatus(1);
-        }else {
-            jsonResult.setStatus(0);
-        }
-        return jsonResult;
+    public JsonResult soundUpload(){
+
+        return null;
     }
-
-
-    @RequestMapping("movieCapsulePut")
-    @ResponseBody
-    public JsonResult movieUpload(HttpServletRequest request) throws IOException {
-        //获取保存在session中的用户信息
-        User user = (User) request.getSession().getAttribute("user");
-        MultipartHttpServletRequest mpRequest = (MultipartHttpServletRequest) request;
-        MultipartFile file = mpRequest.getFile("file");
-        String path = FileTool.uploadFile(file,user);
-        capsule.setCapsulePath(path);
-        capsule.setCapsuleName(request.getParameter("capsuleName"));
-        capsule.setName(request.getParameter("name"));
-        capsule.setEmail(request.getParameter("email"));
-        capsule.setUserId(user.getUserId());
-        capsule.setCapsuleOpenTime(request.getParameter("openTime"));
-        Date date = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");//格式都为2019-03-18
-        capsule.setCapsuleUploadTime(simpleDateFormat.format(date));
-        capsule.setCapsuleTypeId(1);
-        capsule.setOpenPassword(OpenPassword.buildOpenPassword());
-        System.out.println(capsule.toString());
-        if (capsuleService.insertSoundCapsule(capsule)==1){
-            jsonResult.setStatus(1);
-        }else {
-            jsonResult.setStatus(0);
-        }
-        return jsonResult;
-    }
-
-
 }
